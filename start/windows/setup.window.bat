3573b5b80dec1db6e630464bc03b64eae5eab835
MakerDZ <mcskb91@gmail.com>
2023-07-14 22:43:10 +0630

    edit setup bat

--- start/windows/setup.window.bat
+++ start/windows/setup.window.bat
@@ -5,11 +5,17 @@ setlocal enabledelayedexpansion
 REM Change the current directory to the folder where the .bat file is located
 cd /d "%~dp0"
 
+REM install typescript
+call npm install -g typescript
+
 REM install pnpm
 call npm install -g pnpm
 
-REM install packages
+REM install 
 call pnpm install
 
 REM Prevent the terminal from closing
 pause
+
+REM Print success message
+echo All commands executed successfully!
